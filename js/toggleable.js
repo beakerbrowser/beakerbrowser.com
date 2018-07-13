828c3372e5685eb5bcc09da9a59f30809941195d
Paul Frazee <pfrazee@gmail.com>
2018-07-12 20:48:08 -0500

    Improve toggleable behaviors

--- js/toggleable.js
+++ js/toggleable.js
@@ -1,23 +1,20 @@
 document.querySelectorAll('.toggleable-container').forEach(el => el.addEventListener('click', toggle))
-document.querySelectorAll('.toggleable').forEach((el, i) => el.dataset.index = i)
-document.body.addEventListener('click', handleClick)
+document.querySelectorAll('.toggleable').forEach(el => el.addEventListener('click', onClickToggleableItem))
+document.body.addEventListener('click', closeAllToggleables, true)
 
 function toggle (e) {
-  // toggle the current element
-  var currentToggleEl = e.currentTarget.querySelector('.toggleable')
-  currentToggleEl.classList.toggle('visible')
-
-  closeAllToggleables(currentToggleEl)
+  console.log('toggling')
+  e.currentTarget.querySelector('.toggleable').classList.toggle('visible')
 }
 
-function closeAllToggleables (currentToggleEl = null)  {
+function onClickToggleableItem (e) {
+  e.stopPropagation()
+  closeAllToggleables()
+}
+
+function closeAllToggleables ()  {
+  console.log('clsoing all')
   document.querySelectorAll('.toggleable').forEach(el => {
-    if (!currentToggleEl || (el.dataset.index !== currentToggleEl.dataset.index)) {
-      el.classList.remove('visible')
-    }
+    el.classList.remove('visible')
   })
-}
-
-function handleClick (e) {
-  console.log(e.target)
 }
